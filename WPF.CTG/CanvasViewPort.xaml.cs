3954b0b1ef12b5dae7e8a851129f698e0283cff4
Sergey Maklakov <maklakovsb@yandex.ru>
2018-08-02 11:43:35 +0300

    Добавлен новый класс.

---
+++ WPF.CTG/CanvasViewPort.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF.CTG
+{
+    /// <summary>
+    /// Interaction logic for CanvasViewPort.xaml
+    /// </summary>
+    public partial class CanvasViewPort : Canvas, INotifyPropertyChanged
+    {
+        public CanvasViewPort()
+        {
+            DataContext = this;
+            InitializeComponent();
+            //SizeChanged += HandleSizeChanged;
+        }
+
+        //public static DependencyProperty RealWidthProperty = DependencyProperty.Register(
+        //    nameof(RealWidth), 
+        //    typeof(double),
+        //    typeof(CanvasViewPort),
+        //    new PropertyMetadata(500D));
+
+        //public double RealWidth
+        //{
+        //    get { return (double)GetValue(RealWidthProperty); }
+        //    set { SetValue(RealWidthProperty, value); }
+        //}
+
+        //private void HandleSizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    RealWidth = e.NewSize.Width;
+        //}
+
+        #region Реализация интерфейсов
+
+        /// <summary>
+        /// Реализация интерфейса INotifyPropertyChanged
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
---
+++ WPF.CTG/Converters/WidthToMaxConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF.CTG.Converters
+{
+    public class WidthToMaxConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            var widthWithScaling = (values[0] != DependencyProperty.UnsetValue && 
+                                    values[1] != DependencyProperty.UnsetValue) ? (double)values[0] : 0.0;
+            var coordinateViewPortActualWidth = (values[0] != DependencyProperty.UnsetValue && 
+                                                 values[1] != DependencyProperty.UnsetValue) ? (double)values[1] : 0.0;
+            return widthWithScaling - (coordinateViewPortActualWidth - 2);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
