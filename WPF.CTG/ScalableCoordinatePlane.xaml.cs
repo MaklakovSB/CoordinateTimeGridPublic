057d8591652bb9f826da988683fed4fe89f25b94
Sergey Maklakov <maklakovsb@yandex.ru>
2018-08-15 16:31:41 +0300

    Удалёны более не используемые свойства зависимости и методы.

--- WPF.CTG/ScalableCoordinatePlane.xaml.cs
+++ WPF.CTG/ScalableCoordinatePlane.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System.ComponentModel;
-using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 
 namespace WPF.CTG
@@ -32,7 +30,6 @@ namespace WPF.CTG
             typeof(ScalableCoordinatePlane),
             new PropertyMetadata(0.0, OnOriginalWidthPropertyChange));
 
-
         /// <summary>
         /// Цвет кисти разметочной сетки.
         /// </summary>
@@ -41,42 +38,6 @@ namespace WPF.CTG
             typeof(Color),
             typeof(ScalableCoordinatePlane),
             new PropertyMetadata(Colors.Transparent, OnGridColorPropertyChange));
-
-        /// <summary>
-        /// Смещение координатной плоскости по оси X.
-        /// </summary>
-        public static readonly DependencyProperty MoveXProperty = DependencyProperty.Register(
-            nameof(MoveX),
-            typeof(double),
-            typeof(ScalableCoordinatePlane),
-            new PropertyMetadata(0.0, OnMoveXPropertyChange));
-
-        /// <summary>
-        /// Смещение координатной плоскости по оси Y.
-        /// </summary>
-        public static readonly DependencyProperty MoveYProperty = DependencyProperty.Register(
-            nameof(MoveY),
-            typeof(double),
-            typeof(ScalableCoordinatePlane),
-            new PropertyMetadata(0.0, OnMoveYPropertyChange));
-
-        /// <summary>
-        /// Кордината X опорной точки масштабирования координатной плоскости.
-        /// </summary>
-        public static readonly DependencyProperty ScaleCenterXProperty = DependencyProperty.Register(
-            nameof(ScaleCenterX),
-            typeof(double),
-            typeof(ScalableCoordinatePlane),
-            new PropertyMetadata(0.0, OnScaleCenterXPropertyChange));
-
-        /// <summary>
-        /// Кордината Y опорной точки масштабирования координатной плоскости.
-        /// </summary>
-        public static readonly DependencyProperty ScaleCenterYProperty = DependencyProperty.Register(
-            nameof(ScaleCenterY),
-            typeof(double),
-            typeof(ScalableCoordinatePlane),
-            new PropertyMetadata(0.0, OnScaleCenterYPropertyChange));
 
         /// <summary>
         /// Коэффициент масштаба по оси X.
@@ -110,7 +71,7 @@ namespace WPF.CTG
         #region Акцессоры свойств зависимости
 
         /// <summary>
-        /// 
+        /// Оригинальная ширина.
         /// </summary>
         public double OriginalWidth
         {
@@ -119,7 +80,7 @@ namespace WPF.CTG
         }
 
         /// <summary>
-        /// 
+        /// Оригинальная высота.
         /// </summary>
         public double OriginalHeight
         {
@@ -134,42 +95,6 @@ namespace WPF.CTG
         {
             get { return (Color) GetValue(GridColorProperty); }
             set { SetValue(GridColorProperty, value); }
-        }
-
-        /// <summary>
-        /// Смещение по оси X
-        /// </summary>
-        public double MoveX
-        {
-            get { return (double)GetValue(MoveXProperty); }
-            set { SetValue(MoveXProperty, value); }
-        }
-
-        /// <summary>
-        /// Смещение по оси Y
-        /// </summary>
-        public double MoveY
-        {
-            get { return (double)GetValue(MoveYProperty); }
-            set { SetValue(MoveYProperty, value); }
-        }
-
-        /// <summary>
-        /// Кордината X опорной точки масштабирования.
-        /// </summary>
-        public double ScaleCenterX
-        {
-            get { return (double)GetValue(ScaleCenterXProperty); }
-            set { SetValue(ScaleCenterXProperty, value); }
-        }
-
-        /// <summary>
-        /// Кордината Y опорной точки масштабирования.
-        /// </summary>
-        public double ScaleCenterY
-        {
-            get { return (double)GetValue(ScaleCenterYProperty); }
-            set { SetValue(ScaleCenterYProperty, value); }
         }
 
         /// <summary>
@@ -211,24 +136,9 @@ namespace WPF.CTG
         private double _scaleRate = 1;
 
         /// <summary>
-        /// Геометрия разметочной сетки.
-        /// </summary>
-        private GeometryDrawing _markingGridGeometry;
-
-        /// <summary>
         /// Кисть разметочной сетки.
         /// </summary>
         private SolidColorBrush _coordinateGridColor;
-
-        /// <summary>
-        /// Трансформация смещения.
-        /// </summary>
-        private TranslateTransform _translateTransform;
-
-        /// <summary>
-        /// Трансформация масштабирования.
-        /// </summary>
-        private ScaleTransform _scaleTransform;
 
         /// <summary>
         /// Родительский канвас
@@ -284,115 +194,21 @@ namespace WPF.CTG
         public ScalableCoordinatePlane() : base() 
         {
             DataContext = this;
-            //base.Height = OriginalHeight;
-            //base.Width = OriginalWidth;
-            
             InitializeComponent();
+
             //MarkingGridInitialize();
-            //МodifierInitialize();
         }
 
         #endregion
 
         #region Методы
 
-        //private int size = 10; // do something less hardcoded
-
-        //protected override void OnRender(DrawingContext dc)
-        //{
-        //    Pen pen = new Pen(Brushes.AliceBlue, 0.1);
-
-        //    // vertical lines
-        //    double pos = 0;
-        //    int count = 0;
-        //    do
-        //    {
-        //        dc.DrawLine(pen, new Point(pos * ScaleRateX, 0), new Point(pos * ScaleRateX, DesiredSize.Height));
-        //        pos += size * ScaleRateX;
-        //        count++;
-        //    }
-        //    while (pos * ScaleRateX < DesiredSize.Width);
-
-        //    //string title = count.ToString();
-
-        //    // horizontal lines
-        //    pos = 0;
-        //    count = 0;
-        //    do
-        //    {
-        //        dc.DrawLine(pen, new Point(0, pos), new Point(DesiredSize.Width, pos));
-        //        pos += size * ScaleRateY;
-        //        count++;
-        //    }
-        //    while (pos < DesiredSize.Height);
-
-        //    // display the grid size (debug mode only!)
-        //    //title += "x" + count;
-        //    //dc.DrawText(new FormattedText(title, CultureInfo.InvariantCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 20, Brushes.White), new Point(0, 0));
-        //}
-
-        //protected override Size MeasureOverride(Size availableSize)
-        //{
-        //    return availableSize;
-        //}
-
         /// <summary>
         /// Инициализация сетки на заднем фоне координатной плоскости.
         /// </summary>
         private void MarkingGridInitialize()
         {
-            ////var rG1 = new RectangleGeometry();
-            ////rG1.Rect = new Rect(1, 1, 10, 10);
-
-            ////var rG2 = new RectangleGeometry();
-            ////rG2.Rect = new Rect(1.2, 1.2, 9.8, 9.8);
-
-            //var P1 = new Path() {Stroke = _coordinateGridColor};
-
-            //var lG1 = new Line()
-            //{
-                 
-            //};
-
-           
-
-            //var lG2 = new LineGeometry()
-            //{
-            //    StartPoint = new Point() { X = 0, Y = 10 },
-            //    EndPoint = new Point() { X = 320, Y = 10 },
-            //    StrokeContein
-            //};
-
-
-
-
-            //var gG = new GeometryGroup() { FillRule = FillRule.EvenOdd };
-            //gG.Children.Add(lG1);
-            //gG.Children.Add(lG2);
-
-            //_markingGridGeometry = new GeometryDrawing() { Brush = _coordinateGridColor = new SolidColorBrush() {Color = new Color() {A=255, R=255,G=255,B=255} } };
-            //_markingGridGeometry.Geometry = gG;
-
-            //var dB = new DrawingBrush()
-            //{
-            //    TileMode = TileMode.FlipXY,
-            //    Viewport = new Rect(0, 0, 10, 10),
-            //    ViewportUnits = BrushMappingMode.Absolute
-            //};
-
-            //dB.Drawing = _markingGridGeometry;
-            //this.Background = dB;
-        }
-
-        /// <summary>
-        /// Инициализация трансформаций.
-        /// </summary>
-        private void МodifierInitialize()
-        {
-            var tg = new TransformGroup();
-            tg.Children.Add(_translateTransform = new TranslateTransform());
-            tg.Children.Add(_scaleTransform = new ScaleTransform());
-            RenderTransform = tg;
+            ;
         }
 
         #endregion
@@ -411,7 +227,6 @@ namespace WPF.CTG
             {
                 obj._originalHeight = (double)e.NewValue;
                 obj.Height = obj._originalHeight * obj._scaleRateY;
-                //obj.OnPropertyChanged(nameof(Height));
             }
         }
 
@@ -427,8 +242,6 @@ namespace WPF.CTG
             {
                 obj._originalWidth = (double)e.NewValue;
                 obj.Width = obj._originalWidth * obj._scaleRateX;
-                //obj.OnPropertyChanged(nameof(Width));
-
             }
         }
 
@@ -445,62 +258,6 @@ namespace WPF.CTG
                 //var scb = (SolidColorBrush)obj._markingGridGeometry.Brush;
                 //scb.Color = (Color)e.NewValue;
                 obj.GridColor = (Color)e.NewValue;
-            }
-        }
-
-        /// <summary>
-        /// Изменение смещения по оси X
-        /// </summary>
-        /// <param name="d"></param>
-        /// <param name="e"></param>
-        private static void OnMoveXPropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var obj = d as ScalableCoordinatePlane;
-            if (obj != null)
-            {
-                //obj._translateTransform.X = (double) e.NewValue;
-            }
-        }
-
-        /// <summary>
-        /// Изменение смещения по оси Y
-        /// </summary>
-        /// <param name="d"></param>
-        /// <param name="e"></param>
-        private static void OnMoveYPropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var obj = d as ScalableCoordinatePlane;
-            if (obj != null)
-            {
-                //obj._translateTransform.Y = (double) e.NewValue;
-            }
-        }
-
-        /// <summary>
-        /// Изменение кординаты X опорной точки масштабирования.
-        /// </summary>
-        /// <param name="d"></param>
-        /// <param name="e"></param>
-        private static void OnScaleCenterXPropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var obj = d as ScalableCoordinatePlane;
-            if (obj != null)
-            {
-                //obj._scaleTransform.CenterX = (double)e.NewValue;
-            }
-        }
-
-        /// <summary>
-        /// Изменение кординаты Y опорной точки масштабирования.
-        /// </summary>
-        /// <param name="d"></param>
-        /// <param name="e"></param>
-        private static void OnScaleCenterYPropertyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var obj = d as ScalableCoordinatePlane;
-            if (obj != null)
-            {
-                //obj._scaleTransform.CenterY = (double)e.NewValue;
             }
         }
 
@@ -534,7 +291,6 @@ namespace WPF.CTG
             }
         }
 
-        
         /// <summary>
         /// Изменение коэффициента масштаба по оси Y.
         /// </summary>
